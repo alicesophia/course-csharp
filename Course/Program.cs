a0fe09928e3977c80d01835853b3dea32b2c3b10
alicesophia <alicesophiakoch@gmail.com>
2022-02-06 20:46:54 -0300

    Exemplo GetHashCode e Equals

--- Course/Program.cs
+++ Course/Program.cs
@@ -10,7 +10,21 @@ namespace Course {
 
         static void Main(string[] args) {
 
+            Client client1 = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client client2 = new Client { Name = "Alex", Email = "alex@gmail.com" };
 
+            Console.WriteLine(client1.Equals(client2));
+            //O == compara a referência do ponteiro de memória do objeto, então com o mesmo email os clientes vão ser diferentes usando o ==
+            Console.WriteLine(client1 == client2);
+            Console.WriteLine(client1.GetHashCode());
+            Console.WriteLine(client2.GetHashCode());
+
+            //string a = "Maria";
+            //string b = "José";
+            
+            //Para cada objeto é gerado um hash code diferente
+            //Console.WriteLine(a.GetHashCode());
+            //Console.WriteLine(b.GetHashCode());
 
             //Trabalhando com arquivos
 
