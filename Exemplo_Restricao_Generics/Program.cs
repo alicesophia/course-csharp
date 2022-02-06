4c3825a6764c130ff21fcacdff702d0ee998531a
alicesophia <alicesophiakoch@gmail.com>
2022-02-06 19:48:34 -0300

    Exemplo restricao Generics

---
+++ Exemplo_Restricao_Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Exemplo_Restricao_Generics.Services;
+using Exemplo_Restricao_Generics.Entities;
+using System.Globalization;
+
+namespace Exemplo_Restricao_Generics {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++) {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Max: ");
+            Console.WriteLine(max);
+
+        }
+    }
+}
---
+++ Exemplo_Restricao_Generics/Services/CalculationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_Restricao_Generics.Services {
+    class CalculationService {
+
+        //A lista passada como argumento deve ser do tipo T e deve ser IComparable
+        public T Max<T>(List<T> list) where T : IComparable {
+            if (list.Count == 0) {
+                throw new ArgumentException("This list can not be empty");
+            }
+
+            T max = list[0];
+            for (int i = 1; i < list.Count; i++) {
+                if (list[i].CompareTo(max) > 0) {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+
+    }
+}
