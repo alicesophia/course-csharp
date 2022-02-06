a75ef177d856c5e1f4c57df1bc7d79732245a072
alicesophia <alicesophiakoch@gmail.com>
2022-02-06 00:47:46 -0300

    Exemplo IComparable

---
+++ Exemplo_usando_Interface_IComparable/Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exemplo_usando_Interface_IComparable.Entities {
+    class Employee : IComparable {
+
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee) {
+            string[] vetor = csvEmployee.Split(',');
+            Name = vetor[0];
+            Salary = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString() {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj) { 
+            if (!(obj is Employee)) {
+                throw new ArgumentException("Comparing error: argument is not an Employee");
+            }
+
+            Employee other = obj as Employee;
+
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ Exemplo_usando_Interface_IComparable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Exemplo_usando_Interface_IComparable.Entities;
+
+namespace Exemplo_usando_Interface_IComparable {
+    class Program {
+        static void Main(string[] args) {
+
+            string path = @"C:\temp\in.txt";
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    List<Employee> list = new List<Employee>();    
+                    while (!sr.EndOfStream) {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    //Sort ordena a lista, mas o objeto da lista tem que implementar interface IComparable
+                    list.Sort();
+                    foreach (Employee emp in list) {
+                        Console.WriteLine(emp);
+                    }
+                }
+            } catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
