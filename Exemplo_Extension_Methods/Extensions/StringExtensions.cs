d47bb0d048787c0f34b0b3e6e6f140dcf65ea838
alicesophia <alicesophiakoch@gmail.com>
2022-02-07 00:27:33 -0300

    Exemplo Extension Methods

---
+++ Exemplo_Extension_Methods/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+namespace System {
+    static class StringExtensions {
+
+        public static string Cut(this String thisObj, int count) {
+            if (thisObj.Length <= count) {
+                return thisObj;
+            } else {
+                return thisObj.Substring(0, count) + "...";
+            }
+        }
+
+    }
+}
---
+++ Exemplo_Extension_Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exemplo_Extension_Methods {
+    class Program {
+        static void Main(string[] args) {
+
+            DateTime dt = new DateTime(2022, 2, 6, 8, 0, 45);
+
+            //Na hora de chamar o método, não é necessário passar nada porque na declaração do método já existe uma referência a ele mesmo
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+
+        }
+    }
+}
