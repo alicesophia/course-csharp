5ece23daf229b7e10c1d01dafd6cf90711e04ab9
alicesophia <alicesophiakoch@gmail.com>
2022-02-06 23:51:40 -0300

    Exercicio de fixacao sobre Dictionary

---
+++ Exercicio_fixacao_Dictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Exercicio_fixacao_Dictionary {
+    class Program {
+        static void Main(string[] args) {
+
+            Dictionary<string, int> votes = new Dictionary<string, int>();
+
+            string path = @"C:\temp\in.txt";
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] line = sr.ReadLine().Split(',');
+                        
+                        string name = line[0];                        
+                        int vote = int.Parse(line[1]);
+                        
+                        if (votes.ContainsKey(name)) {
+                            votes[name] += vote;
+                        } else {
+                            votes[name] = vote;
+                        }
+
+                    }
+                    
+                    foreach (KeyValuePair<string, int> i in votes) {
+                        Console.WriteLine($"{i.Key}: {i.Value}");
+                    }
+
+                }
+            } catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
