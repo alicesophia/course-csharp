603f7724c41096ef7151c8ef1654ad3dd83b999d
alicesophia <alicesophiakoch@gmail.com>
2022-02-09 02:05:25 -0300

    Exemplo Linq com Lambda

---
+++ Exemplo_Linq_com_Lambda/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Exemplo_Linq_com_Lambda.Entities {
+    class Category {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+    }
+}
