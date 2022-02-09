4b87e9cdbe3df772c96f5d93e4f74cee3f35b6d2
alicesophia <alicesophiakoch@gmail.com>
2022-02-09 02:24:38 -0300

    Exemplo Linq com notacao similar a SQL

--- Exemplo_Linq_com_Lambda/Program.cs
+++ Exemplo_Linq_com_Lambda/Program.cs
@@ -34,7 +34,64 @@ namespace Exemplo_Linq_com_Lambda {
                 new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            //LINQ COM NOTAÇÃO SIMILAR A SQL
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900
+                select p;
+            Print("TIER 1 AND PRICE < 900:", r1);
+
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 =
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
+            Print("NAMES OF PRODUCTS FROM TOOLS", r2);
+
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new {
+                    p.Name,
+                    p.Price,
+                    CategoryName = p.Category.Name
+                };
+            Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
+
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name
+                orderby p.Price
+                select p;
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
+
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 =
+                (from p in r4
+                 select p)
+                 .Skip(2)
+                 .Take(4);
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
+
+            //var r16 = products.GroupBy(p => p.Category);
+            var r16 =
+                from p in products
+                group p by p.Category;
+            foreach (IGrouping<Category, Product> group in r16) {
+                Console.WriteLine("Category " + group.Key.Name + ":");
+                foreach (Product p in group) {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine();
+            }
+            //LINQ COM NOTAÇÃO SIMILAR A SQL
+
+            //LINQ COM LAMBDA
+            /*var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
             Print("TIER 1 AND PRICE < 900: ", r1);
 
             var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
@@ -90,7 +147,8 @@ namespace Exemplo_Linq_com_Lambda {
                     Console.WriteLine(p);
                 }
                 Console.WriteLine();
-            }
+            }*/
+            //LINQ COM LAMBDA
 
             //Criar um data source (coleção, array, recurso de E/S, etc.)
             /*int[] numbers = new int[] { 2, 3, 4, 5 };
